24bc3bb98c74746683d4d545caa4a7eb3d6c2a03
fovlin <fovlin@163.com>
2026-08-04 15:30:13 +0000

    update

---
+++ .vitepress/vue/Connect.vue
@@ -0,0 +1,19 @@
+<script setup>
+    defineProps(['name', 'connect'])
+</script>
+
+<template>
+    <div class="connect-grid">
+        <p id="name">{{ name }}</P>
+        <p id="connect">{{ connect }}</p>
+    </div>
+</template>
+
+<style scoped>
+
+    .connect-grid {
+        border-right: 1.2px solid #ffaf00;
+        font-size: larger;
+    }
+
+</style>
