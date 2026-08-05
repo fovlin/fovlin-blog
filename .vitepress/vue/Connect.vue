6e06455cfcf760519514f5df5a0fee16fad14ac6
fovlin <fovlin@163.com>
2026-08-05 18:31:04 +0800

    update

--- .vitepress/vue/Connect.vue
+++ .vitepress/vue/Connect.vue
@@ -18,7 +18,7 @@
 
     .connect-grid {
         display: grid;
-        grid-template-columns: 3fr 7fr;
+        grid-template-columns: 1fr 3fr;
         align-items: center;
         font-size: 21px;
         text-decoration: none;
@@ -32,22 +32,27 @@
 
     .connect-grid:hover > .connect::before {
         z-index: -1;
+        opacity: 100%;
         width: 100%;
     }
 
     .name {
         transition: 0.3s;
+        margin-right: 12px;
         min-width: 120px;
-        border-right: 3px solid #ffaf00;
+        border-radius: 12px;
+        background-color: var(--vp-c-bg-soft);
         text-align: center;
     }
 
     .connect {
         position: relative;
+        border-left: 5px solid #ffaf00;
         padding-left: 16px;
     }
 
     .connect::before {
+        opacity: 0%;
         z-index: -1;
         transition: 0.3s;
         content: "";
@@ -63,7 +68,7 @@
 
     .connect-grid {
         display: grid;
-        grid-template-columns: 3fr 7fr;
+        grid-template-columns: 1fr 3fr;
         align-items: center;
         font-size: 16px;
     }
